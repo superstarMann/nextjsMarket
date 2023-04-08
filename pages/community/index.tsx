0a3e831aef878b99c2bd05e0dff5b176ea56fdb1
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2023-04-08 17:11:42 +0200

    css

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,20 +1,19 @@
 import { NextPage } from "next";
 
-
 const Community: NextPage = () => {
     return (
         <div className="py-16 space-y-8 px-4">
           <div className="flex flex-col items-start">
             <span className="flex px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                모든 질문
+                Alle Fragen
             </span>
             <div className="mt-2 text-gray-700">
               <span className="text-orange-500 font-medium">
-                Q.</span> What is the best mandu restaurant?
+                Q.</span> Wo ist der berühmteste Friseur in Frankfurt?
             </div>
             <div className="mt-5 flex items-center justify-end w-full font-medium text-xs">
               <span className="text-gray-900 pr-2">{`ID: ${"Lee" /*userName */}`}</span>
-              <span className="text-gray-500">18시간 전</span>
+              <span className="text-gray-500">18h</span>
             </div>
             <div className="flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[1.5px] w-full">
               <span className="flex space-x-2 items-center text-sm ">
@@ -32,7 +31,7 @@ const Community: NextPage = () => {
                     d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
-                <span>궁금해요 1</span>
+                <span>Ich auch 1</span>
               </span>
               <span className="flex space-x-2 items-center text-sm ">
                 <svg
@@ -49,7 +48,7 @@ const Community: NextPage = () => {
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   ></path>
                 </svg>
-                <span>답변 1</span>
+                <span>Antwort 1</span>
               </span>
             </div>
           </div>
